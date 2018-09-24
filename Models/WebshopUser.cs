d72fcf17531a3802c060dc4fb2d69f9c319463fa
Maikel Veen <veenmaikel@gmail.com>
2018-09-24 02:29:06 +0200

    Change User model to WebshopUser model

---
+++ Models/WebshopUser.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Project_Bier.Models
+{
+    public class WebshopUser : IdentityUser
+    {
+        public Guid UserGuid { get; set; }
+
+        [PersonalData]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        public string LastName { get; set; }
+
+        [PersonalData]
+        public DateTime BirthDay { get; set; }
+
+        [PersonalData]
+        public List<ShippingAddress> ShippingAddresses { get; set; }
+
+        public List<FavoriteList> FavoriteLists { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
