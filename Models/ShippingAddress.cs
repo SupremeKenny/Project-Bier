a5d1fe4255c0b1d692c821039feda31ffb44e75c
Maikel Veen <veenmaikel@gmail.com>
2018-09-24 02:29:25 +0200

    Add Shipping Address Model

---
+++ Models/ShippingAddress.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_Bier.Models 
+{
+    /// <summary>  
+    ///  Class encapsulating a physical address
+    /// </summary>  
+    public class ShippingAddress
+    {
+        [Required]
+        public string PostalCode {get; set;}
+
+        [Required]
+        public string StreetNumber {get; set;}
+
+        [Required]
+        public string StreetName {get; set;}
+
+        [Required]
+        public string CityName {get; set;}
+
+        [Required]
+        public string Country {get; set;}
+    }
+}
