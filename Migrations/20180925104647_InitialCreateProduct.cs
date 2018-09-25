14b217ddc70342fbced1f238884d35bba1e1fa9a
Wessel_Schuurman(Laptop) <wessel.schuurman@hotmail.com>
2018-09-25 12:51:05 +0200

    add initial product migration

---
+++ Migrations/20180925104647_InitialCreateProduct.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectBier.Migrations
+{
+    public partial class InitialCreateProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
