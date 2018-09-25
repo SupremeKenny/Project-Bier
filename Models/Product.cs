247031ae9fc9bf296982a88b79e963ebe213b8a9
Wessel_Schuurman(Laptop) <wessel.schuurman@hotmail.com>
2018-09-25 12:50:17 +0200

    added product model

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Project_Bier.Models
+{
+    /// <summary>
+    /// Base class for Product
+    /// </summary>
+    public class Product
+    {
+            [Key]
+            public Guid ProductGuid { get; set; } 
+            public string Name {get; set;}
+            public double Price {get; set;}
+            public Boolean Available {get; set;}
+            public string Description {get; set;} 
+            public string Brand { get; set;}
+    }
+}
