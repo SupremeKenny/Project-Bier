dc2ea45f671ea63175895fdb7849b452a31d6736
Maikel Veen <veenmaikel@gmail.com>
2018-09-27 21:20:34 +0200

    Edit Navigation

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,55 +1,19 @@
 import "semantic-ui-css/semantic.min.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import React, { Component } from "react";
 import {
   Button,
   Container,
-  Divider,
-  Grid,
-  Header,
+  Input,
   Icon,
   Image,
-  List,
   Menu,
   Responsive,
   Segment,
   Sidebar,
   Visibility
 } from "semantic-ui-react";
-
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as="h1"
-      content="Imagine-a-Company"
-      inverted
-      style={{
-        fontSize: mobile ? "2em" : "4em",
-        fontWeight: "normal",
-        marginBottom: 0,
-        marginTop: mobile ? "1.5em" : "3em"
-      }}
-    />
-    <Header
-      as="h2"
-      content="Do whatever you want when you want to."
-      inverted
-      style={{
-        fontSize: mobile ? "1.5em" : "1.7em",
-        fontWeight: "normal",
-        marginTop: mobile ? "0.5em" : "1.5em"
-      }}
-    />
-    <Button primary size="huge">
-      Get Started
-      <Icon name="right arrow" />
-    </Button>
-  </Container>
-);
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool
-};
 
 class DesktopContainer extends Component {
   state = {};
@@ -68,42 +32,53 @@ class DesktopContainer extends Component {
           onBottomPassed={this.showFixedMenu}
           onBottomPassedReverse={this.hideFixedMenu}
         >
-          <Segment
-            inverted
-            textAlign="center"
-            style={{ minHeight: 700, padding: "1em 0em" }}
-            vertical
-          >
+          <Segment inverted textAlign="center" vertical>
             <Menu
               fixed={fixed ? "top" : null}
               inverted={!fixed}
               pointing={!fixed}
               secondary={!fixed}
               size="large"
+              style={{ color: "#2f3542" }}
             >
               <Container>
-                <Menu.Item as="a" active>
-                  Home
+                <Menu.Item>
+                  <Link to="/">
+                    <Image src="logo.png" />
+                  </Link>
                 </Menu.Item>
-                <Menu.Item as="a">Work</Menu.Item>
-                <Menu.Item as="a">Company</Menu.Item>
-                <Menu.Item as="a">Careers</Menu.Item>
-                <Menu.Item position="right">
-                  <Button as="a" inverted={!fixed}>
-                    Log in
-                  </Button>
-                  <Button
-                    as="a"
-                    inverted={!fixed}
-                    primary={fixed}
-                    style={{ marginLeft: "0.5em" }}
-                  >
-                    Sign Up
-                  </Button>
+
+                <Menu.Item style={{ flexGrow: 1 }} >
+                  <Input
+                    action={{
+                      color: "teal",
+                      labelPosition: "right",
+                      icon: "search",
+                      content: "Zoek"
+                    }}
+                    actionPosition="right"
+                    placeholder="Zoeken naar producten..."
+                    
+                  />
                 </Menu.Item>
-              </Container>
+
+                <Menu.Item >
+                  <Link to="/">
+                    <Button primary={!fixed}>Inloggen</Button>
+                  </Link>
+                  <Link to="/">
+                    <Button icon color="red">
+                      <Icon name="heart" />
+                    </Button>
+                  </Link>
+                  <Link to="/">
+                    <Button icon color="yellow">
+                      <Icon name="shop" />
+                    </Button>
+                  </Link>
+                </Menu.Item>
+             </Container>
             </Menu>
-            <HomepageHeading />
           </Segment>
         </Visibility>
 
@@ -179,7 +154,6 @@ class MobileContainer extends Component {
                   </Menu.Item>
                 </Menu>
               </Container>
-              <HomepageHeading mobile />
             </Segment>
 
             {children}
