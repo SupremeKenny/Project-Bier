1f5de762fb43aef2f25f74779bf9f80afc65b052
Maikel Veen <veenmaikel@gmail.com>
2018-11-14 16:15:53 +0100

    Change SearchController to make use of the ISearchService
    Will be injected by the platform

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -6,51 +6,29 @@ using Microsoft.AspNetCore.Mvc;
 using Project_Bier.Repository;
 using Microsoft.AspNetCore.Http;
 using Project_Bier.Models;
-using Nest;
-using Elasticsearch;
+using Project_Bier.Services;
 
 namespace Project_Bier.Controllers
 {
-
     [Route("[controller]/[action]")]
     public class SearchController : Controller
     {
+        ElasticSearchService ElasticSearchService { get; }
+
+        public SearchController(ElasticSearchService elasticSearchService) => ElasticSearchService = elasticSearchService;
+
         [HttpGet]
-        public IActionResult SearchFuzzy(String query)
+        public IActionResult Search(String id)
         {
-            ConnectionSettings settings = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("products");
-            ElasticClient client = new ElasticClient(settings);
+            IEnumerable<Product> results = ElasticSearchService.Search(id);
+            return Json(results);
+        }
 
-            var searchResponse = client.Search<Beer>(s => s.Query(
-                q => q.MultiMatch(c => c
-                    .Fields(f => f.Field(p=>p.Name).Field(p => p.CategoryId))
-                    .Query(query)
-                    .Analyzer("standard")
-                    .Boost(1.1)
-                    .Slop(2)
-                    .Fuzziness(Fuzziness.Auto)
-                    .PrefixLength(2)
-                    .MaxExpansions(2)
-                    .Operator(Operator.Or)
-                    .MinimumShouldMatch(2)
-                    .FuzzyRewrite(MultiTermQueryRewrite.ConstantScoreBoolean)
-                    .TieBreaker(1.1)
-                    .CutoffFrequency(0.001)
-                    .Lenient()
-                    .ZeroTermsQuery(ZeroTermsQuery.All)     
-                    )
-                )
-            );
-
-            IEnumerable<Product> products;
-            if(searchResponse.Hits.Count > 0) 
-            {
-                products = searchResponse.Hits.Select(h => h.Source);
-                return Json(new {product = products});
-            }
-            else {
-                return NotFound();
-            }
+        [HttpGet]
+        public IActionResult Suggest(String id)
+        {
+            SuggestResponse results = ElasticSearchService.Suggest(id);
+            return Json(results);
         }
     }
 }
