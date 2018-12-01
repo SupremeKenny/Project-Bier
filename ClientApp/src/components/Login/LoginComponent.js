155ae04e5cf0ecf1a0dca02fef9492c3d20971a0
Maikel Veen <veenmaikel@gmail.com>
2018-12-01 22:48:11 +0100

    Add user interface for login
    The files only include (limited) validation and presentational code. It is not coupled to the database yet.

---
+++ ClientApp/src/components/Login/LoginComponent.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import {
+    Button,
+    Divider,
+    Form
+} from "semantic-ui-react";
+
+export class LoginComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            userHasFocused: {
+                email: false,
+                password: false
+            },
+            validationState: {
+                email: false,
+                password: false
+            }
+        }
+    }
+
+    handleChange = (e, { name, value }) => this.setState({ ...this.state, [name]: value });
+    handleBlur = (field) => (e) => {
+        this.validateForm();
+        this.setState({ userHasFocused: { ...this.state.userHasFocused, [field]: true } });
+    }
+
+    validateForm() {
+        let emailValid = !this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        let passwordValid = this.state.password.length === 0 ||this.state.password.length <= 8;
+
+        this.setState({ validationState: { email: emailValid, password: passwordValid } });
+    }
+
+    handleSubmit() {
+
+    }
+
+    // TODO:
+    // Make this function less verbose
+    shouldMarkError(field) {
+        switch (field) {
+            case "email":
+                if (this.state.userHasFocused.email == true) {
+                    return (this.state.validationState.email) ? "error" : "";
+                } else return "";
+            case "password":
+                if (this.state.userHasFocused.password == true) {
+                    return (this.state.validationState.password) ? "error" : "";
+                } else return "";
+            default:
+                return "";
+        }
+    }
+
+    render() {
+        const { email, password } = this.state;
+
+        return (
+            <Form onSubmit={this.handleSubmit} >
+
+                <Form.Input
+                    className={this.shouldMarkError("email")} 
+                    placeholder="Emailadres"
+                    name="email"
+                    width={12}
+                    value={email}
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur("email")}
+                />
+
+                <Form.Input
+                    className={this.shouldMarkError("password")} 
+                    placeholder='Wachtwoord'
+                    name="password"
+                    width={12}
+                    value={password}
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur("password")}
+                />
+                <Divider hidden />
+
+                <Button positive>Inloggen</Button>
+                <a href="/">Wachtwoord vergeten?</a>
+            </Form>);
+    }
+}
---
+++ ClientApp/src/components/Login/LoginPage.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { LoginComponent } from "./LoginComponent.js"
+
+import {
+  Container,
+  Segment,
+  Grid,
+  Button,
+  Header,
+  Form,
+  Message
+} from "semantic-ui-react";
+
+export class LoginPage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <Message
+          style={{ marginTop: '1em' }}
+          header='Je wachtwoord alsjeblieft.'
+          content='Om de gegevens in je account goed te kunnen beschermen, vragen wij je om een wachtwoord.' />
+        <Header as="h1"> Inloggen</Header>
+        <Container>
+          <Segment placeholder padded='very' size={'big'} key={'large'}>
+            <Grid columns={2} stackable >
+
+              <Grid.Row >
+
+                <Grid.Column>
+                  <Header as="h3"> Bestaande klanten</Header>
+                  <LoginComponent />
+                </Grid.Column>
+
+                <Grid.Column>
+                  <Header as="h3"> Nieuw bij BeerBuddy?</Header>
+                  <p>Maak binnen enkele minuten een nieuw account aan!</p>
+
+                  <Form onSubmit={this.handleSubmit} >
+                    <Form.Input
+                      placeholder="Emailadres"
+                      name="email"
+                      width={12}
+                    />
+                    <Button positive href="/input">Maak een account</Button>
+                  </Form>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </Container>
+      </div>
+    );
+  }
+}
