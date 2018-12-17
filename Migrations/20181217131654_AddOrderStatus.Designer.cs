19fe294a06cd51c58c280ac08bb38f817ace1c59
Maikel Veen <veenmaikel@gmail.com>
2018-12-17 14:18:09 +0100

    Add OrderStatus to order model

--- Migrations/20181217131654_AddOrderStatus.Designer.cs
+++ Migrations/20181217131654_AddOrderStatus.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Project_Bier.Repository;
@@ -9,9 +10,10 @@ using Project_Bier.Repository;
 namespace ProjectBier.Migrations
 {
     [DbContext(typeof(ApplicationDatabaseContext))]
-    partial class ApplicationDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20181217131654_AddOrderStatus")]
+    partial class AddOrderStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -251,8 +253,6 @@ namespace ProjectBier.Migrations
 
                     b.Property<DateTime>("OrderShipped");
 
-                    b.Property<int>("OrderStatus");
-
                     b.Property<bool>("OrderedFromGuestAccount");
 
                     b.Property<bool>("Paid");
@@ -260,6 +260,8 @@ namespace ProjectBier.Migrations
                     b.Property<bool>("Shipped");
 
                     b.Property<decimal>("TotalPrice");
+
+                    b.Property<int>("orderStatus");
 
                     b.HasKey("Guid");
 
---
+++ Migrations/20181217131654_AddOrderStatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectBier.Migrations
+{
+    public partial class AddOrderStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "orderStatus",
+                table: "Order",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "orderStatus",
+                table: "Order");
+        }
+    }
+}
