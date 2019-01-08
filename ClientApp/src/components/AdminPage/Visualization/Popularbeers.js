50a375b7f175f4fd1f259b975bb57ce4aff4a9a0
Wessel_Schuurman(Laptop) <wessel.schuurman@hotmail.com>
2019-01-08 13:12:05 +0100

    nice design

--- ClientApp/src/components/AdminPage/Visualization/Popularbeers.js
+++ ClientApp/src/components/AdminPage/Visualization/Popularbeers.js
@@ -1,62 +1,62 @@
 import React from "react";
-import {
-    Header,
-    Segment,
-    Container,
-    Dimmer,
-    Loader
-} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Header, Segment, Container, Dimmer, Loader, Grid, List } from "semantic-ui-react";
 
 export class Popularbeers extends React.Component {
-    state = {
-        loaded: false,
-    }
+  state = {
+    loaded: false
+  };
 
-    componentWillMount (){
-        fetch("/Statitics/FetchPopularBeers")
-        .then(results => {
-            results.json().then(data => {
-                console.log(data);
-                
+  componentWillMount() {
+    fetch("/Statitics/FetchPopularBeers").then(results => {
+      results.json().then(data => {
+        console.log(data);
 
-            this.setState({
-                popularbeers: data.popularbeers,
-                loaded: true
-            })
-          });
+        this.setState({
+          popularbeers: data.popularbeers,
+          loaded: true
         });
-        
-    }
+      });
+    });
+  }
 
+  render() {
+    if (!this.state.loaded) {
+      return (
+        <Dimmer active inverted>
+          <Loader size="large">Loading</Loader>
+        </Dimmer>
+      );
+    } else
+      return (
+        <Container>
+          <Header as="h1">Best verkochte biertjes</Header>
+          <Segment>
+            <Grid columns={1}>
+              <Grid.Column>
+                <List divided verticalAlign="bottom" size={"medium"}>
+                    {Object.keys(this.state.popularbeers).map((key, index) => (
+                    <List.Item key={index}>
+                      <List.Content floated="right">
+                        
+                          
+                      </List.Content>
 
-    render() {
-
-
-        if (!this.state.loaded) {
-            return (
-              <Dimmer active inverted>
-                <Loader size="large">Loading</Loader>
-              </Dimmer>
-            );
-        } else
-        return (
-            <Container>
-                <Header as='h1'>Best verkochte biertjes</Header>
-                <Segment>
-                    
-                    {
-                        Object.keys(this.state.popularbeers).map((key, index) => ( 
-                            <p key={index}> {key} is {this.state.popularbeers[key]} keer verkocht.</p> 
-                            ))
-                    }
-                   
-                </Segment>
-
-                
-
-            </Container>
-        );
-    }
-
+                      <List.Content verticalAlign="bottom">
+                        <Header as="h4">
+                          <Link to={"/product/" + key}>
+                            {key}
+                          </Link>
+                        </Header>
+                        {this.state.popularbeers[key]} keer verkocht
+                      </List.Content>
+                    </List.Item>
+                  ))}
+                </List>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Container>
+      );
+  }
 }
-
